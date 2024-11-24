005727d4fc3622989eb07e5d164c470a1f7bfaa4
Igor Serov <igor.serov@contractors.twinformatics.at>
2024-11-24 17:03:24 +0100

    add implementation

--- src/main/java/com/prodyna/dop/task/Contract.java
+++ src/main/java/com/prodyna/dop/task/Contract.java
@@ -4,15 +4,12 @@ import com.prodyna.dop.task.Insurance;
 
 public class Contract {
 
-
     private static final double OLD_TIMER_CAR_INSURANCE_MULTIPLIER = 2.0;
     private static final double NORMAL_CAR_CAR_INSURANCE_MULTIPLIER = 0.5;
     private static final double DANGEROUS_JOB_INSURANCE_MULTIPLIER = 1.1;
     private static final double DEFAULT_PERSON_INSURANCE_MULTIPLIER = 1.0;
     private static final double YOUNG_PERSON_INSURANCE_MULTIPLIER = 0.9;
 
-
-    //TODO implement me using Pattern Matching: Record Patterns, Unnamed Patterns and Guarded Patterns
     /**
      * Calculates the price of the given insurance based on its type and attributes.
      *
@@ -20,6 +17,17 @@ public class Contract {
      * @return the calculated price of the insurance
      */
     public Double getPrice(Insurance insurance) {
-        return 0.0;
+        return switch (insurance) {
+            case Insurance.CarInsurance(Integer co2, Integer hp, Boolean isOldTimer) when isOldTimer ->
+                    OLD_TIMER_CAR_INSURANCE_MULTIPLIER * co2 * hp;
+            case Insurance.CarInsurance(Integer co2, Integer hp, _) -> NORMAL_CAR_CAR_INSURANCE_MULTIPLIER * co2 * hp;
+            case Insurance.HousholdInsurance housholdInsurance ->
+                    housholdInsurance.livingArea() * housholdInsurance.numberOfLivingPeople();
+            case Insurance.LifeInsurance(Integer age, Boolean hasDangerousJob) when hasDangerousJob ->
+                    DANGEROUS_JOB_INSURANCE_MULTIPLIER * age;
+            case Insurance.LifeInsurance(Integer age, _) when age < 33 -> YOUNG_PERSON_INSURANCE_MULTIPLIER * age;
+            case Insurance.LifeInsurance(Integer age, _) -> DEFAULT_PERSON_INSURANCE_MULTIPLIER * age;
+
+        };
     }
 }
--- src/main/java/com/prodyna/dop/task/Insurance.java
+++ src/main/java/com/prodyna/dop/task/Insurance.java
@@ -1,7 +1,42 @@
 package com.prodyna.dop.task;
-/*
+
+import java.util.Objects;
+
 public sealed interface Insurance {
 
-    //TODO add records for CarInsurance, LifeInsurance, HousholdInsurance
+    record CarInsurance(Integer co2, Integer hp, Boolean isOldTimer) implements Insurance {
+        public CarInsurance {
+            Objects.requireNonNull(co2);
+            Objects.requireNonNull(hp);
+            if (hp <= 0)
+                throw new IllegalArgumentException("hp should not be negative");
+            Objects.requireNonNull(isOldTimer);
+
+        }
+    }
+
+    record LifeInsurance(Integer age,
+                         Boolean hasDangerousJob) implements Insurance {
+        public LifeInsurance {
+            Objects.requireNonNull(age);
+            if (age < 18 || age > 70)
+                throw new IllegalArgumentException("age should be between 18 and 70");
+            Objects.requireNonNull(hasDangerousJob);
+        }
+    }
+
+    record HousholdInsurance(Double livingArea,
+                             Boolean isHouse,
+                             Integer numberOfLivingPeople) implements Insurance {
+        public HousholdInsurance {
+            Objects.requireNonNull(livingArea);
+            if (livingArea < 0)
+                throw new IllegalArgumentException("livingArea should not be negative");
+            Objects.requireNonNull(isHouse);
+            Objects.requireNonNull(numberOfLivingPeople);
+            if (numberOfLivingPeople < 1 || numberOfLivingPeople > 10)
+                throw new IllegalArgumentException("numberOfLivingPeople should be between 0 and 10");
+        }
+    }
+
 }
-*/
